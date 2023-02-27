66c12c3fef45c5508551ece5f36e4cc50aa3b16b
Samuel Baraka <sababuvercetti@gmail.com>
2023-02-27 19:24:24 +0300

    feat: added get rows function

--- lib/database_client/database_client.dart
+++ lib/database_client/database_client.dart
@@ -4,8 +4,22 @@ import 'package:sqflite/sqflite.dart';
 class DatabaseClient {
   Future<Map<String, dynamic>> saveToDb(
       {required Map<String, dynamic> data}) async {
-    var database = GetIt.I.get<Database>();
-    await database.insert('amitruck_table', {'data': data.toString()});
-    return data;
+    try {
+      var database = GetIt.I.get<Database>();
+      await database.insert('amitruck_table', {'data': data.toString()});
+      return data;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getRows() async {
+    try {
+      var database = GetIt.I.get<Database>();
+      List<Map<String, dynamic>> rows = await database.query('amitruck_table');
+      return rows;
+    } catch (e) {
+      throw e;
+    }
   }
 }
