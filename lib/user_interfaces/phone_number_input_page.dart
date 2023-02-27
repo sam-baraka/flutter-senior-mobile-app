9ab413eb67c0ab49f2bb405c12537e033fb08c6d
Samuel Baraka <sababuvercetti@gmail.com>
2023-02-27 14:19:11 +0300

    feat: Added the phone number input page

---
+++ lib/user_interfaces/phone_number_input_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PhoneNumberInputPage extends ConsumerWidget {
+  const PhoneNumberInputPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
