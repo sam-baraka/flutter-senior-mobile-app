f7ca74a7271a950f6d06c40a9d8d09b8ac3afffe
Samuel Baraka <sababuvercetti@gmail.com>
2023-02-27 14:43:02 +0300

    FEAT: MORE WIDGET TESTS

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:interview_amitruck/main.dart';
+import 'package:interview_amitruck/user_interfaces/phone_code_input_page.dart';
 import 'package:interview_amitruck/user_interfaces/phone_number_input_page.dart';
 
 void main() {
@@ -35,6 +36,38 @@ void main() {
       expect(
           find.byType(
             PhoneNumberInputPage,
+          ),
+          findsOneWidget);
+    });
+  });
+
+  group('Phone number input page tests', () {
+    testWidgets('The phone input page shows successfully',
+        (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const MaterialApp(
+        home: PhoneNumberInputPage(),
+      ));
+
+      // Verify that our counter starts at 0.
+      expect(find.text('Enter your phone number to Join Amitruck'),
+          findsOneWidget);
+// Verify that we have a multiple buttons
+      expect(find.byType(MaterialButton), findsWidgets);
+    });
+
+    testWidgets('Tapping on the button navigates to the phone code input page',
+        (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const MaterialApp(
+        home: PhoneNumberInputPage(),
+      ));
+
+      await tester.tap(find.text('Confirm and Continue'));
+      await tester.pumpAndSettle();
+      expect(
+          find.byType(
+            PhoneCodeInputPage,
           ),
           findsOneWidget);
     });
