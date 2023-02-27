065d50ec7b9e6aa607518ebfb6ceec7dc92db41f
Samuel Baraka <sababuvercetti@gmail.com>
2023-02-27 15:12:24 +0300

    feat: Added form validation

--- lib/user_interfaces/phone_code_input_page.dart
+++ lib/user_interfaces/phone_code_input_page.dart
@@ -1,67 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:interview_amitruck/user_interfaces/order_form_page.dart';
 import 'package:pin_input_text_field/pin_input_text_field.dart';
 
 class PhoneCodeInputPage extends ConsumerWidget {
-  const PhoneCodeInputPage({super.key});
-
+  PhoneCodeInputPage({super.key});
+  final formKey = GlobalKey<FormBuilderState>();
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
         body: Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(
-            height: 200,
-          ),
-          const Text(
-            'Enter your phone number to Join Amitruck',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 18),
-          ),
-          FormBuilderField(
-              builder: (field) {
-                return PinInputTextField(
-                    pinLength: 4,
-                    decoration: UnderlineDecoration(
-                      colorBuilder:
-                          PinListenColorBuilder(Colors.purple, Colors.purple),
-                      obscureStyle: ObscureStyle(
-                        obscureText: '😂',
-                      ),
-                    ));
+      child: FormBuilder(
+        key: formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              height: 200,
+            ),
+            const Text(
+              'Enter your phone number to Join Amitruck',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18),
+            ),
+            FormBuilderField(
+                validator: FormBuilderValidators.compose(
+                    [FormBuilderValidators.required()]),
+                builder: (field) {
+                  return PinInputTextField(
+                      pinLength: 4,
+                      onChanged: (value) {
+                        field.didChange(value);
+                      },
+                      decoration: UnderlineDecoration(
+                        colorBuilder:
+                            PinListenColorBuilder(Colors.purple, Colors.purple),
+                        obscureStyle: ObscureStyle(
+                          obscureText: '😂',
+                        ),
+                      ));
+                },
+                name: 'code'),
+            MaterialButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return OrderFormPage();
+                }));
               },
-              name: 'code'),
-          MaterialButton(
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                return const PhoneCodeInputPage();
-              }));
-            },
-            color: Colors.purple,
-            child: const Text(
-              'Validate and Continue',
-              style: TextStyle(color: Colors.white),
+              color: Colors.purple,
+              child: const Text(
+                'Validate and Continue',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-          ),
-          const Expanded(child: SizedBox()),
-          MaterialButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            color: Colors.grey,
-            child: const Text(
-              '< Go back',
-              style: TextStyle(color: Colors.white),
+            const Expanded(child: SizedBox()),
+            MaterialButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              color: Colors.grey,
+              child: const Text(
+                '< Go back',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-        ],
+            const SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
       ),
     ));
   }
--- lib/user_interfaces/phone_number_input_page.dart
+++ lib/user_interfaces/phone_number_input_page.dart
@@ -1,67 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:form_builder_phone_field/form_builder_phone_field.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:interview_amitruck/user_interfaces/phone_code_input_page.dart';
 
 class PhoneNumberInputPage extends ConsumerWidget {
-  const PhoneNumberInputPage({super.key});
-
+  PhoneNumberInputPage({super.key});
+  final formKey = GlobalKey<FormBuilderState>();
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
         body: Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(
-            height: 200,
-          ),
-          const Text(
-            'Enter your phone number to Join Amitruck',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 18),
-          ),
-          FormBuilderPhoneField(
-            name: 'phone_number',
-            defaultSelectedCountryIsoCode: 'KE',
-            decoration: const InputDecoration(
-              labelText: 'Phone Number',
-              hintText: '712345782',
+      child: FormBuilder(
+        key: formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              height: 200,
             ),
-            priorityListByIsoCode: const ['KE'],
-            validator: FormBuilderValidators.compose([
-              FormBuilderValidators.required(),
-            ]),
-          ),
-          MaterialButton(
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                return const PhoneCodeInputPage();
-              }));
-            },
-            color: Colors.purple,
-            child: const Text(
-              'Confirm and Continue',
-              style: TextStyle(color: Colors.white),
+            const Text(
+              'Enter your phone number to Join Amitruck',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18),
             ),
-          ),
-          const Expanded(child: SizedBox()),
-          MaterialButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            color: Colors.grey,
-            child: const Text(
-              '< Go back',
-              style: TextStyle(color: Colors.white),
+            FormBuilderPhoneField(
+              name: 'phone_number',
+              defaultSelectedCountryIsoCode: 'KE',
+              decoration: const InputDecoration(
+                labelText: 'Phone Number',
+                hintText: '712345782',
+              ),
+              priorityListByIsoCode: const ['KE'],
+              validator: FormBuilderValidators.compose([
+                FormBuilderValidators.required(),
+              ]),
             ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-        ],
+            MaterialButton(
+              onPressed: () {
+                if (formKey.currentState!.saveAndValidate()) {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return PhoneCodeInputPage();
+                  }));
+                }
+              },
+              color: Colors.purple,
+              child: const Text(
+                'Confirm and Continue',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            const Expanded(child: SizedBox()),
+            MaterialButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              color: Colors.grey,
+              child: const Text(
+                '< Go back',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
       ),
     ));
   }
