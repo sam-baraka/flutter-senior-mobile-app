f9b26dc4894c1f18b9bebbc7bca8c4d13ba86391
Samuel Baraka <sababuvercetti@gmail.com>
2023-02-27 13:57:22 +0300

    test: Welcome page text test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:interview_amitruck/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('Welcome page tests', () {
+    testWidgets('The first page that appears is the Welcome page',
+        (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      // Verify that our counter starts at 0.
+      expect(find.text('Welcome to Amitruck Driver'), findsOneWidget);
+    });
   });
 }
