66ba4b30562b5f3ec7bb6564f985ed6bf46c6add
Samuel Baraka <sababuvercetti@gmail.com>
2023-02-27 18:56:05 +0300

    reqistered database as a singleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:get_it/get_it.dart';
+import 'package:sqflite/sqflite.dart';
 import 'user_interfaces/welcome_page.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final database = await openDatabase('amitruck_database.db', version: 1,
+      onCreate: (Database db, int version) async {
+    await db.execute(
+        'CREATE TABLE amitruck_table (id INTEGER PRIMARY KEY, data TEXT)');
+  });
+
+  /// Register the database as a singleton
+  GetIt.I.registerSingleton<Database>(database);
+
   runApp(const MyApp());
 }
 
@@ -12,16 +26,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: WelcomePage(),
-      supportedLocales: [
-        Locale('en'),
-      ],
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        FormBuilderLocalizations.delegate,
-      ],
+    return const ProviderScope(
+      child: MaterialApp(
+        home: WelcomePage(),
+        supportedLocales: [
+          Locale('en'),
+        ],
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          FormBuilderLocalizations.delegate,
+        ],
+      ),
     );
   }
 }
