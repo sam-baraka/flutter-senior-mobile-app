a24f0850553e969017a6cf9f7ef9026c3fa3d290
Samuel Baraka <sababuvercetti@gmail.com>
2023-02-27 14:44:29 +0300

    implemented the phone input page

--- lib/user_interfaces/phone_number_input_page.dart
+++ lib/user_interfaces/phone_number_input_page.dart
@@ -1,11 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:form_builder_phone_field/form_builder_phone_field.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:interview_amitruck/user_interfaces/phone_code_input_page.dart';
 
 class PhoneNumberInputPage extends ConsumerWidget {
   const PhoneNumberInputPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
+            height: 200,
+          ),
+          const Text(
+            'Enter your phone number to Join Amitruck',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18),
+          ),
+          FormBuilderPhoneField(
+            name: 'phone_number',
+            defaultSelectedCountryIsoCode: 'KE',
+            decoration: const InputDecoration(
+              labelText: 'Phone Number',
+              hintText: '712345782',
+            ),
+            priorityListByIsoCode: const ['KE'],
+            validator: FormBuilderValidators.compose([
+              FormBuilderValidators.required(),
+            ]),
+          ),
+          MaterialButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                return const PhoneCodeInputPage();
+              }));
+            },
+            color: Colors.purple,
+            child: const Text(
+              'Confirm and Continue',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          const Expanded(child: SizedBox()),
+          MaterialButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            color: Colors.grey,
+            child: const Text(
+              '< Go back',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+        ],
+      ),
+    ));
   }
 }
